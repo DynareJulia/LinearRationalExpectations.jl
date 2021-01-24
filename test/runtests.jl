98981fe2da0a3b5e0504b527454bac174b20836c
MichelJuillard <michel.juillard@mjui.fr>
2021-01-24 10:45:52 +0100

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using LinearRationalExpectations
+using LinearAlgebra
 using Random
 using Test
 
-#include("linear_rational_expectations_test.jl")
+include("linear_rational_expectations_test.jl")
 include("extended_lyapunov_test.jl")
