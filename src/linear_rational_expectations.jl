9d871a8e199d79d04ed672e451057fcf39d09dc7
MichelJuillard <michel.juillard@mjui.fr>
2021-04-18 15:40:13 +0200

    bug fixes + optimizations

--- src/linear_rational_expectations.jl
+++ src/linear_rational_expectations.jl
@@ -51,6 +51,8 @@ struct LinearRationalExpectationsWs
     temp5::Matrix{Float64}
     temp6::Matrix{Float64}
     temp7::Matrix{Float64}
+    temp8::Matrix{Float64}
+    temp9::Matrix{Float64}
     b10::Matrix{Float64}
     b11::Matrix{Float64}
     icolsD::Vector{Int64}
@@ -123,6 +125,8 @@ struct LinearRationalExpectationsWs
         temp5 = Matrix{Float64}(undef, endogenous_nbr, exogenous_nbr)
         temp6 = Matrix{Float64}(undef, static_nbr, backward_nbr)
         temp7 = Matrix{Float64}(undef, endogenous_nbr, backward_nbr)
+        temp8 = Matrix{Float64}(undef, endogenous_nbr, forward_nbr)
+        temp9 = Matrix{Float64}(undef, endogenous_nbr, current_nbr)
         b10 = Matrix{Float64}(undef, static_nbr,static_nbr)
         b11 = Matrix{Float64}(undef, static_nbr, endogenous_nbr - static_nbr)
         current_backward_indices = findall(in(backward_indices), current_indices)
@@ -133,10 +137,10 @@ struct LinearRationalExpectationsWs
         icolsD = [k1;  backward_nbr .+ (1:forward_nbr)]
         jcolsD = [backward_nbr .+ current_backward_indices;
                   backward_nbr + current_nbr .+ (1:forward_nbr)]
-        k2 = findall(in(current_indices), forward_indices)
-        k2a = findall(in(purely_forward_indices), forward_indices[k2])
+        k2a = findall(in(purely_forward_indices), forward_indices)
+        k2b = findall(in(purely_forward_indices), current_indices)
         icolsE = [1:backward_nbr; backward_nbr .+ k2a]
-        jcolsE = [1:backward_nbr; backward_nbr .+ forward_indices[k2a]]
+        jcolsE = [1:backward_nbr; backward_nbr .+ k2b]
         colsUD = findall(in(forward_indices), backward_indices)
         colsUE = backward_nbr .+ findall(in(backward_indices), forward_indices)
         linsolve_static_ws = LinSolveWs(static_nbr)
@@ -156,8 +160,8 @@ struct LinearRationalExpectationsWs
             static_nbr, dynamic_nbr, forward_nbr, backward_nbr,
             both_nbr, current_nbr, jacobian_static, qr_ws, solver_ws,
             a, b, c, d, e, x,
-            ghx, gx, hx, temp1, temp2, temp3, temp4, temp5, temp6, temp7, b10, b11,
-            icolsD, icolsE, jcolsD, jcolsE,
+            ghx, gx, hx, temp1, temp2, temp3, temp4, temp5, temp6, temp7,
+            temp8, temp9, b10, b11, icolsD, icolsE, jcolsD, jcolsE,
             colsUD, colsUE, AGplusB, linsolve_static_ws, AGplusB_linsolve_ws)
     end
 end
@@ -166,10 +170,12 @@ struct LinearRationalExpectationsResults
     g1::Matrix{Float64}  # full approximation
     gs1::Matrix{Float64} # state transition matrices
     g1_1::SubArray # solution first order derivatives w.r. to state variables
-    g1_2::SubArray # solution first order derivatives w.r. to current exogenous variables
+     g1_2::SubArray # solution first order derivatives w.r. to current exogenous variables
 #    g1_3::SubArray # solution first order derivatives w.r. to lagged exogenous variables
     
-    function LinearRationalExpectationsResults(order::Int64, endogenous_nbr::Int64, exogenous_nbr::Int64, backward_nbr::Int64)
+    function LinearRationalExpectationsResults(endogenous_nbr::Int64,
+                                               exogenous_nbr::Int64,
+                                               backward_nbr::Int64)
         nstate = backward_nbr + exogenous_nbr 
         g1 =  zeros(endogenous_nbr,(nstate + 1))
         gs1 = zeros(backward_nbr,backward_nbr)
@@ -206,52 +212,52 @@ function add_static!(results::LinearRationalExpectationsResults,
                      ws::LinearRationalExpectationsWs)
     # static rows are at the top of the QR transformed Jacobian matrix
     # B_s,s
-    @inbounds for i = 1:static_nbr
-        k = ws.backward_nbr + ws.static_indices[i]
+    fill!(ws.b10, 0.0)
+    @inbounds for i = 1:ws.static_nbr
+        k = ws.static_indices_j[i]
         for j = 1:ws.static_nbr
             ws.b10[j, i] = jacobian[j, k]
         end
     end
     # B_s,d
-    @inbounds for i = 1:ws.dynamic_nbr
-        k = ws.backward_nbr + ws.dynamic_indices[i]
+    @inbounds for i = 1:length(ws.current_dynamic_indices)
+        k1 = ws.current_dynamic_indices_d[i]
+        k2 = ws.current_dynamic_indices_d_j[i]
         for j = 1:ws.static_nbr
-            ws.b11[j, i] = jacobian[j, k]
+            ws.b11[j, k1] = jacobian[j, k2]
         end
     end
     # A_s
     @inbounds for i = 1:ws.forward_nbr
-        k = ws.backward_nbr + ws.current_nr + ws.dynamic_indices[i]
+        k = ws.backward_nbr + ws.current_nbr + i
         for j = 1:ws.static_nbr
             ws.temp1[j, i] = jacobian[j, k]
         end
     end
-    @inbounds ws.temp1 .= view(jacobian, i_static, ws.backward_nbr + ws.current_nbr .+ (1:ws.forward_nbr))
     # C_s
     for i = 1:ws.backward_nbr
         for j = 1:ws.static_nbr
             ws.temp2[j, i] = jacobian[j, i]
         end
     end
-    # Gy,fwrd
-    @inbounds for i = 1:backward_nbr
-        k = ws.forward_indices[i]
+    # Gy.fwrd
+    @inbounds for i = 1:ws.backward_nbr
         for j = 1:ws.forward_nbr
-            ws.temp3[j, i] = results.g1_1[ws.forward_indices[j], k]
+            ws.temp3[j, i] = results.g1_1[ws.forward_indices[j], i]
         end
     end
-    # Gy,dynamic
-    @inbounds for i = 1:backward_nbr
-        k = ws.forward_indices[i]
+    # Gy.dynamic
+    @inbounds for i = 1:ws.backward_nbr
         for j = 1:ws.dynamic_nbr
-            ws.temp4[j, i] = results.g1_1[ws.dynamic_indices[j], k]
+            ws.temp4[j, i] = results.g1_1[ws.dynamic_indices[j], i]
         end
     end
     # ws.temp2 = B_s,d*Gy.dynamic + C_s
     mul!(ws.temp2, ws.b11, ws.temp4, 1.0, 1.0)
+    # ws.temp6 = A_s*Gy.fwrd*gs1
     mul!(ws.temp6, ws.temp1, ws.temp3)
     mul!(ws.temp2, ws.temp6, results.gs1, -1.0, -1.0)
-    # ws.temp3 = S\ws.temp3
+    # ws.temp3 = B_s,s\ws.temp2
     linsolve_core!(ws.b10, ws.temp2, ws.linsolve_static_ws)
     @inbounds for i = 1:ws.backward_nbr
         for j=1:ws.static_nbr
@@ -297,13 +303,28 @@ function get_de!(ws::LinearRationalExpectationsWs, jacobian::AbstractMatrix{Floa
     n1 = ws.backward_nbr + ws.forward_nbr - ws.both_nbr
     fill!(ws.d, 0.0)
     fill!(ws.e, 0.0)
-    i_rows = (ws.static_nbr + 1):ws.endogenous_nbr
-    ws.d[1:n1, ws.icolsD] .= jacobian[i_rows, ws.jcolsD]
-    ws.e[1:n1, ws.icolsE] .= -jacobian[i_rows, ws.jcolsE]
-    u = Matrix{Float64}(I, ws.both_nbr, ws.both_nbr)                                    
-    i_rows = n1 .+ (1:ws.both_nbr)
-    ws.d[i_rows, ws.colsUD] .= u
-    ws.e[i_rows, ws.colsUE] .= u
+    for i = 1:length(ws.jcolsD)
+        k = ws.jcolsD[i]
+        for j = 1:ws.dynamic_nbr
+            m = ws.static_nbr + j
+            ws.d[j, ws.icolsD[i]] = jacobian[m, k] 
+        end
+    end
+    for i = 1:length(ws.jcolsE)
+        k = ws.jcolsE[i]
+        for j = 1:ws.dynamic_nbr
+            m = ws.static_nbr + j
+            ws.e[j, ws.icolsE[i]] = -jacobian[m, k] 
+        end
+    end
+    for i = 1:ws.both_nbr
+        k = ws.dynamic_nbr + i
+        ws.d[k, ws.colsUD[i]] = 1.0
+    end
+    for i = 1:ws.both_nbr
+        k = ws.dynamic_nbr + i
+        ws.e[k, ws.colsUE[i]] = 1.0
+    end
 end
 
 """
@@ -311,12 +332,32 @@ Computes LU decomposition of A*G + B
 """
 function make_lu_AGplusB!(AGplusB, A, G, B, ws)
     fill!(AGplusB, 0.0)
+    for i = 1:ws.current_nbr
+        k = ws.current_indices[i]
+        for j = 1:ws.endogenous_nbr
+            AGplusB[j, k] = B[j, i]
+        end
+    end
+    for i = 1:ws.backward_nbr
+        for j = 1:ws.forward_nbr
+            ws.temp3[j, i] = G[ws.forward_indices[j], i]
+        end
+    end
+    mul!(ws.temp7, A, ws.temp3)
+    for i = 1:ws.backward_nbr
+        k = ws.backward_indices[i]
+        for j = 1:ws.endogenous_nbr
+            AGplusB[j, k] += ws.temp7[j, i]
+        end
+    end
+        #=
     vAGplusB = view(AGplusB, :, ws.current_indices)
     vAGplusB .= B
     ws.temp3 .= view(G, ws.forward_indices, :)
     mul!(ws.temp7, A, ws.temp3)
     vAGplusB = view(AGplusB, :, ws.backward_indices)
     vAGplusB .+= ws.temp7
+        =#
     LinSolveAlgo.lu!(AGplusB, ws.AGplusB_linsolve_ws)
 end
 
@@ -333,7 +374,14 @@ function solve_for_derivatives_with_respect_to_shocks!(results::LinearRationalEx
     end
     =#
     if ws.exogenous_nbr > 0
-        results.g1_2 .= .-view(jacobian, :, ws.exogenous_indices)
+        #   results.g1_2 .= .-view(jacobian, :, ws.exogenous_indices)
+        for i = 1:ws.exogenous_nbr
+            k = ws.exogenous_indices[i]
+            for j = 1:ws.endogenous_nbr
+                results.g1_2[j, i] = -jacobian[j, k]
+            end
+        end
+        
 #        if ws.serially_correlated_exogenous
             # TO BE DONE
         #        else
@@ -347,7 +395,9 @@ function first_order_solver!(results::LinearRationalExpectationsResults,
                              jacobian::Matrix,
                              options,
                              ws::LinearRationalExpectationsWs)
+    remove_static!(jacobian, ws)
     if algo == "CR"
+        get_abc!(ws, jacobian)
         cyclic_reduction!(ws.x, ws.c, ws.b, ws.a, ws.solver_ws, options["cyclic_reduction"]["tol"], 100)
         for i = 1:ws.backward_nbr
             for j = 1:ws.backward_nbr
@@ -358,6 +408,7 @@ function first_order_solver!(results::LinearRationalExpectationsResults,
             end
         end
     elseif algo == "GS"
+        get_de!(ws, jacobian)
         gs_solver!(ws.solver_ws, ws.d, ws.e, ws.backward_nbr, options["generalized_schur"]["criterium"])
         results.gs1 .= ws.solver_ws.g1
         for i = 1:ws.backward_nbr
@@ -376,9 +427,21 @@ function first_order_solver!(results::LinearRationalExpectationsResults,
     if ws.static_nbr > 0
         add_static!(results, jacobian, ws)
     end
-    A = view(jacobian, :, ws.backward_nbr + ws.current_nbr .+ (1:ws.forward_nbr))
-    B = view(jacobian, :, ws.backward_nbr .+ ws.current_indices)
-    make_lu_AGplusB!(ws.AGplusB, A, results.g1_1, B, ws)        
+    #    A = view(jacobian, :, ws.backward_nbr + ws.current_nbr .+ (1:ws.forward_nbr))
+    #    B = view(jacobian, :, ws.backward_nbr .+ ws.current_indices)
+    for i = 1:ws.forward_nbr
+        k = ws.backward_nbr + ws.current_nbr + i
+        for j = 1:ws.endogenous_nbr
+            ws.temp8[j, i] = jacobian[j, k]
+        end
+    end
+    for i = 1:ws.current_nbr
+        k = ws.backward_nbr + i
+        for j = 1:ws.endogenous_nbr
+            ws.temp9[j, i] = jacobian[j, k]
+        end
+    end
+    make_lu_AGplusB!(ws.AGplusB, ws.temp8, results.g1_1, ws.temp9, ws)        
     solve_for_derivatives_with_respect_to_shocks!(results, jacobian, ws)
 end
 
--- test/linear_rational_expectations_test.jl
+++ test/linear_rational_expectations_test.jl
@@ -1,4 +1,31 @@
-algo = "CR"
+using Random
+
+function make_jacobian(ws)
+    jacobian_orig = zeros(ws.endogenous_nbr,
+                          ws.backward_nbr
+                          + ws.current_nbr
+                          + ws.forward_nbr
+                          + ws.exogenous_nbr)
+    for i = 1:1000
+        @show i
+        Random.seed!(i)
+        jacobian = randn(ws.endogenous_nbr,
+                          ws.backward_nbr
+                          + ws.current_nbr
+                          + ws.forward_nbr
+                         + ws.exogenous_nbr)
+        copy!(jacobian_orig, jacobian)
+        LinearRationalExpectations.remove_static!(jacobian, ws)
+        LinearRationalExpectations.get_de!(ws, jacobian)
+        F = schur(ws.e, ws.d)
+        if count(abs.(F.α ./ F.β) .> 1.0) == ws.forward_nbr
+            break
+        end
+    end
+    return jacobian_orig
+end
+
+algo = "GS"
 
 endogenous_nbr = 10
 exogenous_nbr = 3
@@ -21,29 +48,69 @@ ws = LinearRationalExpectationsWs(algo,
                                   static_indices)
 @test ws.static_indices_j == [12, 14]
 
-order = 1
-backward_nbr_b = length(backward_indices) + length(both_indices)
-results = LinearRationalExpectationsResults(order,
-                                            endogenous_nbr,
+results = LinearRationalExpectationsResults(endogenous_nbr,
                                             exogenous_nbr,
-                                            backward_nbr_b)
-
-jacobian = randn(endogenous_nbr,
-                 length(forward_indices)
-                 + length(current_indices)
-                 + length(backward_indices)
-                 + 2*length(both_indices)
-                 + exogenous_nbr)
-
+                                            ws.backward_nbr)
+jacobian = make_jacobian(ws)
 jacobian_orig = copy(jacobian)
 LinearRationalExpectations.remove_static!(jacobian, ws)
 FQ = LinearAlgebra.qr(jacobian_orig[:, [12, 14]])
 @test jacobian ≈ transpose(FQ.Q)*jacobian_orig
 
-LinearRationalExpectations.get_abc!(ws, jacobian)
-@test ws.a[:, [2, 3, 5, 7, 8]] == jacobian[3:end, 15:19]
-@test ws.b[:, [2, 3, 4, 5, 7, 8]] == jacobian[3:end, [6, 7, 8, 9, 11, 13]]
-@test ws.c[:, [1, 4, 6, 7, 8]] == jacobian[3:end, 1:5]
+LinearRationalExpectations.get_de!(ws, jacobian)
+@show ws.icolsE
+@show ws.jcolsE
+@test ws.d[1:8, 1] == zeros(8)
+@test ws.d[1:8, 2:10]  == jacobian[3:end, [8, 10, 11, 13, 15, 16, 17, 18, 19]]
+display(ws.e)
+display(-jacobian[3:8,1:9])
+@test ws.e[1:8, 1:8]  == -jacobian[3:end, [1, 2, 3, 4, 5, 6, 7, 9]]
+
+targetD = zeros(2, 10)
+targetD[1, 4] = 1.0
+targetD[2, 5] = 1.0
+targetE = zeros(2, 10)
+targetE[1, 9] = 1.0
+targetE[2, 10] = 1.0
+@test ws.d[9:10,:] == targetD
+@test ws.e[9:10,:] == targetE
+
+d_orig = copy(ws.d)
+e_orig = copy(ws.e)
+options = Dict()
+options["cyclic_reduction"] = Dict([("tol", 1e-8)])
+options["generalized_schur"] = Dict([("criterium", 1+1e-6)])
+LinearRationalExpectations.PolynomialMatrixEquations.gs_solver!(ws.solver_ws, ws.d, ws.e, ws.backward_nbr, options["generalized_schur"]["criterium"])
+@test d_orig * vcat(I(ws.backward_nbr), ws.solver_ws.g2[:, 1:ws.backward_nbr])*ws.solver_ws.g1 ≈ e_orig * vcat(I(ws.backward_nbr), ws.solver_ws.g2[:, 1:ws.backward_nbr])
+
+results.gs1 .= ws.solver_ws.g1
+for i = 1:ws.backward_nbr
+    for j = 1:ws.backward_nbr
+        x = ws.solver_ws.g1[j,i]
+        results.g1[ws.backward_indices[j],i] = x
+    end
+    for j = 1:(ws.forward_nbr - ws.both_nbr)
+        results.g1[ws.purely_forward_indices[j], i] =
+            ws.solver_ws.g2[ws.icolsE[ws.backward_nbr + j] - ws.backward_nbr, i]
+    end
+end
+
+LinearRationalExpectations.add_static!(results, jacobian, ws)
+@test ws.b11[:, 2:8] ≈ jacobian[1:ws.static_nbr, [6, 7, 8, 9, 10, 11, 13]]
+@test results.g1[ws.forward_indices, 1:ws.backward_nbr] ≈ ws.solver_ws.g2
+@test results.g1[ws.backward_indices, 1:ws.backward_nbr] ≈ ws.solver_ws.g1
+b10 = jacobian[1:ws.static_nbr, [12, 14]]
+@show ws.static_indices_j
+display(b10)
+display(ws.b10)
+@test ws.b10 ≈ b10
+target = -b10\(jacobian[1:ws.static_nbr, 15:19]
+               *results.g1_1[ws.forward_indices,:]
+               *results.gs1
+               + jacobian[1:ws.static_nbr, [6, 7, 8, 9, 10, 11, 13]]
+               *results.g1_1[[2, 3, 4, 5, 6, 7, 9], :]
+               + jacobian[1:ws.static_nbr, 1:ws.backward_nbr])
+@test results.g1[ws.static_indices, 1:ws.backward_nbr] ≈ target
 
 A = randn(10, 5)
 B = randn(10, 9) 
@@ -56,6 +123,7 @@ AGplusB[:, [1, 4, 6, 7, 9]] += A*G[[2, 3, 5, 7, 9], :]
 LU = reshape(ws.AGplusB_linsolve_ws.lu, endogenous_nbr, endogenous_nbr)
 U = triu(LU)
 L = tril(LU, -1) + diagm(ones(10))
+
 function swaprow!(x, i, j)
     for k in axes(x, 2)
         x[i, k], x[j, k] = x[j, k], x[i, k]
@@ -67,6 +135,69 @@ for i in axes(target, 2)
 end
 @test target ≈ L*U 
 
+@show ws.exogenous_indices
+@show size(jacobian)
 LinearRationalExpectations.solve_for_derivatives_with_respect_to_shocks!(results, jacobian, ws)
 g1_2 = -AGplusB\jacobian[:, 20:22]
 @test results.g1_2 ≈ g1_2
+
+
+algo = "GS"
+ws = LinearRationalExpectationsWs(algo,
+                                  endogenous_nbr,
+                                  exogenous_nbr,
+                                  exogenous_deterministic_nbr,
+                                  forward_indices,
+                                  current_indices,
+                                  backward_indices,
+                                  both_indices,
+                                  static_indices)
+jacobian = make_jacobian(ws)
+jacobian_orig = copy(jacobian)
+first_order_solver!(results, algo, jacobian, options, ws)
+@test d_orig * vcat(I(ws.backward_nbr), ws.solver_ws.g2[:, 1:ws.backward_nbr])*ws.solver_ws.g1 ≈ e_orig * vcat(I(ws.backward_nbr), ws.solver_ws.g2[:, 1:ws.backward_nbr])
+A2 = jacobian[:, 15:19]
+A1 = jacobian[:, 6:14]
+A0 = jacobian[:, 1:5]
+@test A2[3:10,:] ≈ d_orig[1:8, 6:10]
+@test A1[3:10, [3, 5, 6, 8]] ≈ d_orig[1:8, 2:5]
+@test A1[3:10, [1, 2, 4]] ≈ -e_orig[1:8, 6:8]
+@test A0[3:10, :] ≈ -e_orig[1:8, 1:5]
+@test d_orig[1:8, 1] ≈ zeros(8, 1)
+@test e_orig[1:8, 9:10] ≈ zeros(8, 2)
+@test results.g1[ws.forward_indices, 1:ws.backward_nbr] ≈ ws.solver_ws.g2
+@test results.g1[ws.backward_indices, 1:ws.backward_nbr] ≈ ws.solver_ws.g1
+g1_1 = results.g1_1
+g1_2 = results.g1_2
+gg = g1_1[ws.forward_indices, :]
+@test gg ≈ ws.solver_ws.g2
+display(A2*gg*g1_1[ws.backward_indices,:] + A1*g1_1[ws.current_indices,:] + A0)
+A2 = jacobian_orig[:, 15:19]
+A1 = jacobian_orig[:, 6:14]
+A0 = jacobian_orig[:, 1:5]
+B = jacobian_orig[:, 20:22]
+display(A2*gg*g1_1[ws.backward_indices,:] +
+        A1*g1_1[ws.current_indices,:] + A0)
+
+@test (A2*gg*g1_1[ws.backward_indices,:] +
+       A1*g1_1[ws.current_indices,:] ≈ -A0 )
+@test (A2*gg*g1_2[ws.backward_indices,:] +
+       A1*g1_2[ws.current_indices,:] ≈ -B )
+
+                             
+algo = "CR"
+ws = LinearRationalExpectationsWs(algo,
+                                  endogenous_nbr,
+                                  exogenous_nbr,
+                                  exogenous_deterministic_nbr,
+                                  forward_indices,
+                                  current_indices,
+                                  backward_indices,
+                                  both_indices,
+                                  static_indices)
+LinearRationalExpectations.get_abc!(ws, jacobian)
+@test ws.a[:, [2, 3, 5, 7, 8]] == jacobian[3:end, 15:19]
+@test ws.b[:, [2, 3, 4, 5, 7, 8]] == jacobian[3:end, [6, 7, 8, 9, 11, 13]]
+@test ws.c[:, [1, 4, 6, 7, 8]] == jacobian[3:end, 1:5]
+
+
