357439fd3f74ceeb5df5d7c620004aa7649d2383
MichelJuillard <michel.juillard@mjui.fr>
2022-03-29 16:10:36 +0200

    add tests back

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,6 @@ using LinearAlgebra
 using Random
 using Test
 
-#include("linear_rational_expectations_test.jl")
-#include("extended_lyapunov_test.jl")
+include("linear_rational_expectations_test.jl")
+include("extended_lyapunov_test.jl")
 include("variance_test.jl")
